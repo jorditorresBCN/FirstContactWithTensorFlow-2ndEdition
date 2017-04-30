7fc55eb7306acc75689dd7aacef61381b7ce8062
Jordi Torres <torres@ac.upc.edu>
2017-04-30 17:43:39 +0200

    Create ValidateYourInstallation.py

---
+++ ValidateYourInstallation.py
@@ -0,0 +1,10 @@
+# to remove warnings saying if you build TensorFlow from source it can be faster on your machine
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+import tensorflow as tf
+
+#test
+import tensorflow as tf
+hello = tf.constant('Hello, TensorFlow!')
+sess = tf.Session()
+print(sess.run(hello))
